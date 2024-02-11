48b82586be5d438fa20367a53766852daadfa4fa
Erlon Pereira Rodrigues <erlon.rodrigues@aluno.ifsp.edu.br>
2024-02-11 20:28:12 -0300

    feat(productsSp): implementa a associação do ProductAdapter com o adapter do Spinner productsSp

--- app/src/main/java/br/edu/ifsp/scl/sdm/dummyproducts/ui/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/sdm/dummyproducts/ui/MainActivity.kt
@@ -3,12 +3,21 @@ package br.edu.ifsp.scl.sdm.dummyproducts.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import br.edu.ifsp.scl.sdm.dummyproducts.R
+import br.edu.ifsp.scl.sdm.dummyproducts.adapter.ProductAdapter
 import br.edu.ifsp.scl.sdm.dummyproducts.databinding.ActivityMainBinding
+import br.edu.ifsp.scl.sdm.dummyproducts.model.Product
 
 class MainActivity : AppCompatActivity() {
     private val amb: ActivityMainBinding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
+
+    private val productList: MutableList<Product> =
+        mutableListOf() // lista de produtos (data source)
+    private val productAdapter: ProductAdapter by lazy { // adapter ProductAdapter
+        ProductAdapter(this, productList)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(amb.root)
@@ -17,5 +26,9 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(amb.mainTb.apply {
             title = getString(R.string.app_name) // define o título da action bar
         })
+
+        // associa o adapter ao spinner
+        amb.productsSp.adapter = productAdapter
+
     }
 }
