f36c234e2f9e019e533bad05262aa450d23b8e07
Erlon Pereira Rodrigues <erlon.rodrigues@aluno.ifsp.edu.br>
2024-02-12 09:01:54 -0300

    feat(HttpURLConnection): implementação da requisição de imagens de produto utilizando HttpURLConnection

--- app/src/main/java/br/edu/ifsp/scl/sdm/dummyproducts/ui/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/sdm/dummyproducts/ui/MainActivity.kt
@@ -1,15 +1,22 @@
 package br.edu.ifsp.scl.sdm.dummyproducts.ui
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import br.edu.ifsp.scl.sdm.dummyproducts.R
 import br.edu.ifsp.scl.sdm.dummyproducts.adapter.ProductAdapter
+import br.edu.ifsp.scl.sdm.dummyproducts.adapter.ProductImageAdapter
 import br.edu.ifsp.scl.sdm.dummyproducts.databinding.ActivityMainBinding
 import br.edu.ifsp.scl.sdm.dummyproducts.model.Product
 import br.edu.ifsp.scl.sdm.dummyproducts.model.ProductList
 import com.google.gson.Gson
 import com.google.gson.JsonSyntaxException
+import java.io.BufferedInputStream
 import java.io.IOException
 import java.io.InputStreamReader
 import java.net.HttpURLConnection
@@ -22,8 +29,14 @@ class MainActivity : AppCompatActivity() {
 
     private val productList: MutableList<Product> =
         mutableListOf() // lista de produtos (data source)
-    private val productAdapter: ProductAdapter by lazy { // adapter ProductAdapter
+    private val productAdapter: ProductAdapter by lazy { // instacia adapter ProductAdapter
         ProductAdapter(this, productList)
+    }
+
+    private val productImageList: MutableList<Bitmap> =
+        mutableListOf() // lista de imagens (data source)
+    private val productImageAdapter: ProductImageAdapter by lazy { // instancia adapter ProductImageAdapter
+        ProductImageAdapter(this, productImageList)
     }
 
     // companion object é utilizado para criar membros de classe que são acessíveis diretamente na classe, em vez de em instâncias específicas da classe
@@ -35,18 +48,45 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(amb.root)
 
-        // define quem é a action bar
-        setSupportActionBar(amb.mainTb.apply {
+        setSupportActionBar(amb.mainTb.apply { // define a action bar
             title = getString(R.string.app_name) // define o título da action bar
         })
 
-        // associa o adapter ao spinner
-        amb.productsSp.adapter = productAdapter
+        amb.productsSp.apply {
+            adapter = productAdapter // associa o adapter do ProductAdapter ao spinner
+            onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>?,
+                    view: View?,
+                    position: Int,
+                    id: Long
+                ) {
+                    val size = productImageList.size
+                    productImageList.clear() // limpa o data set do data source
+                    productImageAdapter.notifyItemRangeRemoved(
+                        0,
+                        size
+                    ) // notifica o adapter que foram removidos todos itens do data set
+                    retrieveProductImages(productList[position])
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+                    // não se aplica
+                }
+            }
+        }
+
+        amb.productImagesRv.apply {
+            layoutManager =
+                LinearLayoutManager(this@MainActivity) // define o layout do RecyclerView
+            adapter =
+                productImageAdapter // associa o adapter ProductImageAdapter ao RecyclerView do layout
+        }
 
         // chama a função retrieveProducts()
         retrieveProducts()
 
-    }
+    } // fechamento onCreate
 
     // função para buscar o conteúdo JSON no Web Service e popular o Spinner
     private fun retrieveProducts() =
@@ -97,5 +137,41 @@ class MainActivity : AppCompatActivity() {
                 productsConnection.disconnect()
             }
         }.start()
+
+    // função para buscar as imagens de produto no arquivo JSON
+    private fun retrieveProductImages(product: Product) = Thread {
+        product.images.forEach { imageUrl ->
+            val imageConnection = URL(imageUrl).openConnection() as HttpURLConnection
+            try {
+                if (imageConnection.responseCode == HttpURLConnection.HTTP_OK) {
+                    BufferedInputStream(imageConnection.inputStream).let {
+                        val imageBitmap =
+                            BitmapFactory.decodeStream(it) // decodifica o input stream no formato binário em um bitmap
+
+                        runOnUiThread {
+                            productImageList.add(imageBitmap) // adiciona o Bitmap à lista productImageList
+                            productImageAdapter.notifyItemInserted(productImageList.lastIndex) // notifica o adapter que foi adicionado um novo item na última posição da lista productImageList
+                        }
+                    }
+                } else {
+                    runOnUiThread {
+                        Toast.makeText(
+                            this,
+                            getString(R.string.request_problem),
+                            Toast.LENGTH_SHORT
+                        )
+                            .show()
+                    }
+                }
+            } catch (ioe: IOException) {
+                runOnUiThread {
+                    Toast.makeText(this, getString(R.string.connection_failed), Toast.LENGTH_SHORT)
+                        .show()
+                }
+            } finally {
+                imageConnection.disconnect()
+            }
+        }
+    }.start()
 
 }
